b369c11cd118fd095d92de14b300d6c0ab97380c
TadeoRial <tadeorial@gmail.com>
2022-04-12 18:28:39 -0300

    new AlbumCategoriesComponent

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -2,18 +2,39 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import { RootTabScreenProps } from '../types';
 
-import Album from '../components/Album';
+import AlbumCategory from '../components/AlmbumCategory';
 
-const album = {
+const albumCategory = {
   id: '1',
-  imageUri: 'https://i.scdn.co/image/ab67616d0000b273328e973ede81069ff83d552e',
-  artistsHeadline: 'Los palmeras, enganchado 24 hs.'
-}
+  title: "Categoría",
+  albums: [
+    {
+      id: '1',
+      imageUri: 'https://i.scdn.co/image/ab67616d0000b273328e973ede81069ff83d552e',
+      artistsHeadline: 'Los palmeras, enganchado 24 hs.'
+    },
+    {
+      id: '2',
+      imageUri: 'https://pxb.cdn.elchubut.com.ar/chubut/012022/1643586934016.jpg',
+      artistsHeadline: 'Pablito Lescano, solos de teclado'
+    },
+    {
+      id: '3',
+      imageUri: 'https://i.scdn.co/image/ab6761610000e5eb91c4459dd3c179af1e99f08f',
+      artistsHeadline: 'La Champions Liga'
+    },
+    {
+      id: '4',
+      imageUri: 'https://img5.yna.co.kr/etc/inner/SP/2021/06/16/ASP20210616002600883_01_i_P2.jpg',
+      artistsHeadline: 'BTS'
+    }
+  ]
+};
 
 export default function HomeScreen({ navigation }: RootTabScreenProps<'Home'>) {
   return (
     <View style={styles.container}>
-      <Album album={album} />
+      <AlbumCategory title={albumCategory.title} albums={albumCategory.albums} />
     </View>
   );
 }
