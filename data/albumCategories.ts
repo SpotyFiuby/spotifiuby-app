6dd09b4f3cf66c23c0ae64ba68b7f688ff19d1d2
TadeoRial <tadeorial@gmail.com>
2022-04-12 18:56:09 -0300

    new AlbumCategoriesList

---
+++ data/albumCategories.ts
@@ -0,0 +1,102 @@
+export default [{
+    id: '1',
+    title: "Categoría 1",
+    albums: [
+      {
+        id: '1',
+        imageUri: 'https://i.scdn.co/image/ab67616d0000b273328e973ede81069ff83d552e',
+        artistsHeadline: 'Los palmeras, enganchado 24 hs.'
+      },
+      {
+        id: '2',
+        imageUri: 'https://pxb.cdn.elchubut.com.ar/chubut/012022/1643586934016.jpg',
+        artistsHeadline: 'Pablito Lescano, solos de teclado'
+      },
+      {
+        id: '3',
+        imageUri: 'https://i.scdn.co/image/ab6761610000e5eb91c4459dd3c179af1e99f08f',
+        artistsHeadline: 'La Champions Liga'
+      },
+      {
+        id: '4',
+        imageUri: 'https://img5.yna.co.kr/etc/inner/SP/2021/06/16/ASP20210616002600883_01_i_P2.jpg',
+        artistsHeadline: 'BTS'
+      }
+    ]
+  },{
+    id: '2',
+    title: "Categoría 2",
+    albums: [
+      {
+        id: '1',
+        imageUri: 'https://i.scdn.co/image/ab67616d0000b273328e973ede81069ff83d552e',
+        artistsHeadline: 'Los palmeras, enganchado 24 hs.'
+      },
+      {
+        id: '2',
+        imageUri: 'https://pxb.cdn.elchubut.com.ar/chubut/012022/1643586934016.jpg',
+        artistsHeadline: 'Pablito Lescano, solos de teclado'
+      },
+      {
+        id: '3',
+        imageUri: 'https://i.scdn.co/image/ab6761610000e5eb91c4459dd3c179af1e99f08f',
+        artistsHeadline: 'La Champions Liga'
+      },
+      {
+        id: '4',
+        imageUri: 'https://img5.yna.co.kr/etc/inner/SP/2021/06/16/ASP20210616002600883_01_i_P2.jpg',
+        artistsHeadline: 'BTS'
+      }
+    ]
+  }, {
+    id: '3',
+    title: "Categoría 3",
+    albums: [
+      {
+        id: '1',
+        imageUri: 'https://i.scdn.co/image/ab67616d0000b273328e973ede81069ff83d552e',
+        artistsHeadline: 'Los palmeras, enganchado 24 hs.'
+      },
+      {
+        id: '2',
+        imageUri: 'https://pxb.cdn.elchubut.com.ar/chubut/012022/1643586934016.jpg',
+        artistsHeadline: 'Pablito Lescano, solos de teclado'
+      },
+      {
+        id: '3',
+        imageUri: 'https://i.scdn.co/image/ab6761610000e5eb91c4459dd3c179af1e99f08f',
+        artistsHeadline: 'La Champions Liga'
+      },
+      {
+        id: '4',
+        imageUri: 'https://img5.yna.co.kr/etc/inner/SP/2021/06/16/ASP20210616002600883_01_i_P2.jpg',
+        artistsHeadline: 'BTS'
+      }
+    ]
+  }, {
+    id: '4',
+    title: "Categoría 4",
+    albums: [
+      {
+        id: '1',
+        imageUri: 'https://i.scdn.co/image/ab67616d0000b273328e973ede81069ff83d552e',
+        artistsHeadline: 'Los palmeras, enganchado 24 hs.'
+      },
+      {
+        id: '2',
+        imageUri: 'https://pxb.cdn.elchubut.com.ar/chubut/012022/1643586934016.jpg',
+        artistsHeadline: 'Pablito Lescano, solos de teclado'
+      },
+      {
+        id: '3',
+        imageUri: 'https://i.scdn.co/image/ab6761610000e5eb91c4459dd3c179af1e99f08f',
+        artistsHeadline: 'La Champions Liga'
+      },
+      {
+        id: '4',
+        imageUri: 'https://img5.yna.co.kr/etc/inner/SP/2021/06/16/ASP20210616002600883_01_i_P2.jpg',
+        artistsHeadline: 'BTS'
+      }
+    ]
+  }
+];
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,40 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, FlatList, View } from 'react-native';
 
 import { RootTabScreenProps } from '../types';
 
 import AlbumCategory from '../components/AlmbumCategory';
-
-const albumCategory = {
-  id: '1',
-  title: "Categoría",
-  albums: [
-    {
-      id: '1',
-      imageUri: 'https://i.scdn.co/image/ab67616d0000b273328e973ede81069ff83d552e',
-      artistsHeadline: 'Los palmeras, enganchado 24 hs.'
-    },
-    {
-      id: '2',
-      imageUri: 'https://pxb.cdn.elchubut.com.ar/chubut/012022/1643586934016.jpg',
-      artistsHeadline: 'Pablito Lescano, solos de teclado'
-    },
-    {
-      id: '3',
-      imageUri: 'https://i.scdn.co/image/ab6761610000e5eb91c4459dd3c179af1e99f08f',
-      artistsHeadline: 'La Champions Liga'
-    },
-    {
-      id: '4',
-      imageUri: 'https://img5.yna.co.kr/etc/inner/SP/2021/06/16/ASP20210616002600883_01_i_P2.jpg',
-      artistsHeadline: 'BTS'
-    }
-  ]
-};
+import albumCategories from '../data/albumCategories';
 
 export default function HomeScreen({ navigation }: RootTabScreenProps<'Home'>) {
   return (
     <View style={styles.container}>
-      <AlbumCategory title={albumCategory.title} albums={albumCategory.albums} />
+      <FlatList
+        data={albumCategories}
+        renderItem={({item}) => (
+          <AlbumCategory 
+            title={item.title}
+            albums={item.albums}
+            keyExtractor={(item) => item.id}
+          />
+        )}
+      />
     </View>
   );
 }
