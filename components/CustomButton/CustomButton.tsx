df96041b6b7f638246ab66353cacea217b804c53
fmonpelat <facundo.monpelat@gmail.com>
2022-04-20 12:11:52 -0300

    changed customButton

--- components/CustomButton/CustomButton.tsx
+++ components/CustomButton/CustomButton.tsx
@@ -2,13 +2,12 @@ import React from "react";
 import { Text, Pressable } from "react-native";
 import styles from "./styles";
 
-const CustomButton = ({ onPress, text='', type="PRIMARY", bgColor, fgColor, style={}, styleText={} }) => {
+const CustomButton = ({ onPress, text='', bgColor, fgColor, style={}, styleText={} }) => {
     return (
         <Pressable
             onPress={onPress}
             style={
                 [{...styles.container, ...style},
-                    styles[`container_${type}`],
                     bgColor ? {backgroundColor: bgColor} : {},
                 ]
             }
