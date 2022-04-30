d43c51f37c9458782171ffae3f48dbf1d0119b4e
fmonpelat <facundo.monpelat@gmail.com>
2022-04-30 10:50:15 -0300

    refactored userHandler for Auth Navigation

--- navigation/AuthNavigation.tsx
+++ navigation/AuthNavigation.tsx
@@ -4,8 +4,7 @@ import firebase from '../firebase';
 
 const AuthNavigation = () => {
     const [currentUser, setCurrentUser] = useState(null);
-    const userHandler = user => 
-        user ? setCurrentUser(user) : setCurrentUser(null);
+    const userHandler = (user: any) => setCurrentUser(user ? user: null);
     useEffect(
         () => firebase.auth().onAuthStateChanged(user => userHandler(user)),
         []
