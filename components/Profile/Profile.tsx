a872da423a439fe65e9d648947e983bd1755802c
fmonpelat <facundo.monpelat@gmail.com>
2022-05-25 20:13:09 -0300

    better ui for profile

--- components/Profile/Profile.tsx
+++ components/Profile/Profile.tsx
@@ -4,28 +4,44 @@ import { Pressable, View, Text } from "react-native";
 import { Avatar, Caption, Title } from "react-native-paper";
 import styles from "./styles";
 
+const getProfile = () => {
+  return {
+    name: "John Doe",
+    avatarUrl: "https://cdn0.iconfinder.com/data/icons/body-parts-glyph-silhouettes/300/161845119Untitled-3-512.png",
+    caption: "@johndoe",
+    location: "Buenos Aires, Argentina",
+    phone: "+54 911223344",
+    bio: "I am a huge fan of the new music. I love to listen to new music and I love to share it with my friends.",
+    email: "johndoe@gmail.com",
+    followers: "1.2k",
+    following: "430",
+  };
+};
+
+
 const Profile = ({navigation}: {navigation: any}) => {
+    const { name, avatarUrl, caption, location, phone, email, bio, followers, following } = getProfile();
     return (
         <>
           <View style={styles.userInfoSection}>
             <View style={{flexDirection: 'row', marginTop:15}}>
               <Avatar.Image
-                source={{uri:'https://cdn0.iconfinder.com/data/icons/body-parts-glyph-silhouettes/300/161845119Untitled-3-512.png',}}
+                source={{uri: `${avatarUrl}`,}}
                 size={80}            
               />
-              <View style={{marginLeft: 20}}>
+              <View style={{marginLeft: 30}}>
                 <Title style={[styles.title, {
                   marginTop: 15,
                   marginBottom:5,
                   color: 'white'
                 }]}>
-                  Fan de Palmeras
+                  {name}
                 </Title>
                 <Caption style={[styles.caption, {color: 'white'}]}>
-                  @palmerasFan99
+                  {caption}
                 </Caption>
               </View>
-              <View style={{ marginLeft: 20}}>
+              <View style={{ marginLeft: 90}}>
                 <Pressable onPress={() => {
                   console.debug('Edit Profile button pressed');
                   navigation.navigate('EditProfile');
@@ -35,24 +51,29 @@ const Profile = ({navigation}: {navigation: any}) => {
               </View>
             </View>
           </View>
+          <View>
+            <View style={[styles.userInfoSection, { alignSelf: 'center'}]}>
+              <Text style={{color: "white"}}>{bio}</Text>
+            </View>
+          </View>
           <View style={styles.userInfoSection}>
             <View style= {styles.row}>
-              <Text style={{color:"#777777", marginLeft: 20}}>Buenos Aires, Argentina</Text>
+              <Text style={{color:"#777777", marginLeft: 20}}>{location}</Text>
             </View>
             <View style= {styles.row}>
-              <Text style={{color:"#777777", marginLeft: 20}}>+54-64207868</Text>
+              <Text style={{color:"#777777", marginLeft: 20}}>{phone}</Text>
             </View>
             <View style= {styles.row}>
-              <Text style={{color:"#777777", marginLeft: 20}}>palmeraaaas@gmail.com</Text>
+              <Text style={{color:"#777777", marginLeft: 20}}>{email}</Text>
             </View>
           </View> 
           <View style={styles.infoBoxWrapper}>
             <View style={styles.infoBox}>
-              <Title style={{color:'white'}}>420</Title>
+              <Title style={{color:'white'}}>{followers}</Title>
               <Caption style={{color: 'grey'}}>Seguidores</Caption>
             </View>
             <View style={styles.infoBox}>
-              <Title style={{color:'white'}}>14</Title>
+              <Title style={{color:'white'}}>{following}</Title>
               <Caption style={{color: 'grey'}}>Seguidos</Caption>
             </View>
           </View>
--- components/Profile/styles.ts
+++ components/Profile/styles.ts
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
     },
     infoBoxWrapper: {
       flexDirection: 'row',
-      height: 100,
+      height: 60,
     },
     infoBox: {
       width: '50%',
