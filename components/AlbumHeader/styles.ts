9c32566f3189ecbf31a0f1c2677d0796ea2a4a88
fmonpelat <facundo.monpelat@gmail.com>
2022-07-04 16:09:15 -0300

    added styling for rating on albums

--- components/AlbumHeader/styles.ts
+++ components/AlbumHeader/styles.ts
@@ -16,15 +16,39 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
     },
     creatorContainer: {
-        flexDirection: 'row',
+        flexDirection: 'column',
         margin: 10,
+        alignItems: 'center',
     },
     creator: {
         color: 'lightgray',
         margin: 5,
         fontSize: 20,
     },
+    rating: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+    },
+    ratingPressable: {
+        marginLeft: 20,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        width: '30%',
+        margin: 5,
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: 'lightgray',
+        padding: 5,
+    },
+    ratingEditText: {
+        color: 'white',
+        fontSize: 10,
+        margin: 5,
+    },
     likes: {
+        marginTop: 10,
         color: 'lightgray',
         margin: 5,
         fontSize: 20,
@@ -41,7 +65,57 @@ const styles = StyleSheet.create({
         color: 'white',
         fontSize: 20,
         fontWeight: 'bold',
-    }
+    },
+    //
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22
+      },
+      modalView: {
+        margin: 20,
+        backgroundColor: "black",
+        borderRadius: 20,
+        borderWidth: 1,
+        borderColor: "lightgray",
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+      },
+      modalButton: {
+        marginTop: 10,
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+      },
+      buttonModalCancel: {
+        backgroundColor: "red",
+        marginLeft: 10,
+      },
+      buttonOpen: {
+        backgroundColor: "#F194FF",
+      },
+      buttonClose: {
+        backgroundColor: "#2196F3",
+      },
+      textStyle: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center"
+      },
+      modalText: {
+        marginBottom: 15,
+        textAlign: "center",
+        color: "white"
+      }
 });
 
 export default styles;
