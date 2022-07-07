f99bdcdd6733d1b4164dfe8aca4cc008b72ced09
fmonpelat <facundo.monpelat@gmail.com>
2022-07-07 19:04:30 -0300

    added go back with old style

--- components/Buttons/GoBackButton/GoBackButton.tsx
+++ components/Buttons/GoBackButton/GoBackButton.tsx
@@ -1,7 +1,6 @@
 import { FontAwesome } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
-import { Text, Pressable, TouchableOpacity, View } from "react-native";
+import { TouchableOpacity, View, Button } from "react-native";
 
 const CustomButton = () => {
 
@@ -9,9 +8,11 @@ const CustomButton = () => {
     
     return (
         <TouchableOpacity onPress={() => {navigation.goBack()}}>
-            <View style={{marginTop: 10, marginLeft: 10, flexDirection: "row", alignItems: "center"}} >
-                <FontAwesome name="angle-left" size={40} color="blue" />
-                <Text style={{color: "blue", fontSize: 25, marginLeft: 10}}> Back</Text>
+            <View style={{ alignSelf: 'flex-start', marginLeft: 20, flexDirection: "row", alignItems: "center" }}>
+                <FontAwesome name="angle-left" size={20} color="blue" />
+                <Button title="Back" onPress={() => {
+                    return navigation.goBack();
+                }} />
             </View>
         </TouchableOpacity>
     );
