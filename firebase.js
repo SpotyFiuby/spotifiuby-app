2d3ebf9978f0434ec99864f339f3cecd858d3700
fmonpelat <facundo.monpelat@gmail.com>
2022-07-07 17:49:04 -0300

    updated firebase with database

--- firebase.js
+++ firebase.js
@@ -3,6 +3,7 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { collection, addDoc, orderBy, query, onSnapshot, getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,7 +17,8 @@ const firebaseConfig = {
   storageBucket: "spotifiuby-b4cf2.appspot.com",
   messagingSenderId: "899600905229",
   appId: "1:899600905229:web:38c3189b6e2f27b99c1144",
-  measurementId: "G-SBDL8SB9BV"
+  measurementId: "G-SBDL8SB9BV",
+  databaseURL: "http://spotifiuby-b4cf2.firebaseio.com"
 };
 
 const storage = {
@@ -25,7 +27,17 @@ const storage = {
   uploadBytes,
   getDownloadURL
 };
+
+const firestore = {
+  collection,
+  addDoc,
+  orderBy,
+  query,
+  onSnapshot
+};
 // Initialize Firebase
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app()
 
-export { firebase, storage }
+const database = getFirestore();
+
+export { firebase, storage, database, firestore }
